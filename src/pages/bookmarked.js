b50ed24e7068ebb3c493116d7e7c9bc81279ff10
Michaelhybrid <michaeloludare15@gmail.com>
2023-05-28 03:08:06 +0100

    Cinematrix

--- src/pages/bookmarked.js
+++ src/pages/bookmarked.js
@@ -1,9 +1,7 @@
-import ViewInfo from './components/ViewInfo';
 import Head from 'next/head';
-import { useInfo } from '../contexts/InfoContext';
 
 export default function Bookmarked() {
-    const { viewInformation, handleBookmark, bookmarks } = useInfo();
+
     return (
         <>
             <Head>
@@ -17,18 +15,8 @@ export default function Bookmarked() {
          lg:h-screen lg:w-[93%] lg:ml-[8%] pb-4'>
                     <p className='pl-4 md:pl-7 pb-4 pt-4 lg:pt-4 text-white text-xl font-light md:text-4xl'>Bookmarked</p>
                     <div className='grid grid-cols-2 gap-4 px-4 md:grid-cols-3 md:gap-5 lg:grid-cols-4 lg:gap-6 md:pl-7 text-white'>
-                        {bookmarks === [] ? <p className='text-white text-lg'>No Bookmarks Yet!</p> : bookmarks.map((movie, index) => (
-                            <div key={index} className='shrink-0 lg:cursor-pointer' onClick={() => viewInformation(movie)}>
-                                <img
-                                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                    alt={movie.title || movie.name}
-                                    className='rounded-xl'
-                                />
-                            </div>
-                        ))}
                     </div>
                 </section>
-                <ViewInfo />
             </main >
         </>
     )
--- src/pages/search-page.js
+++ src/pages/search-page.js
@@ -71,10 +71,10 @@ export default function Search() {
 }
 
 export async function getStaticProps() {
-    const movieGenreRequest = await fetch(`https://cinematrixx.vercel.app/api/genreMovieListApi`);
+    const movieGenreRequest = await fetch(`/api/genreMovieListApi`);
     const movieGenreDetails = await movieGenreRequest.json();
 
-    const tvGenreRequest = await fetch(`https://cinematrixx.vercel.app/api/genreTvListApi`);
+    const tvGenreRequest = await fetch(`/api/genreTvListApi`);
     const tvGenreDetails = await tvGenreRequest.json();
 
     return {
